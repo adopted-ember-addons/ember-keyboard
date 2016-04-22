94e11c141e47b7b5694dc02a91861ac2c4e0a378
null null null <null@revolution.cheap>
2016-04-22 15:24:56 -0700

    add support for alternate key names in IE

--- addon/utils/listener-name.js
+++ addon/utils/listener-name.js
@@ -1,5 +1,17 @@
+import altKeyNames from 'ember-keyboard/fixtures/alt-key-names';
+
+function convertArray(keyArray) {
+  return keyArray.map((key) => altKeyNames[key] || key);
+}
+
+function sortedKeys(keyArray) {
+  const convertedArray = convertArray(keyArray);
+
+  return convertedArray.sort().join('+');
+}
+
 export default function listenerName(type, keyArray = []) {
-  const keys = keyArray.length === 0 ? '_all' : keyArray.sort().join('+');
+  const keys = keyArray.length === 0 ? '_all' : sortedKeys(keyArray);
 
   return `${type}:${keys}`;
 }
