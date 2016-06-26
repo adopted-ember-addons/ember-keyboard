07940604c3c2899063f68662e00642166aa7068f
Ben Limmer <ben@benlimmer.com>
2016-06-26 10:50:47 -0600

    Allow using `cmd` as DSL for ctrl or command key. Users of macOS expect to press Command+Key whereas Windows, Linux, etc. expect to press Ctrl+Key. This allows easily defining that behavior. Fixes #38.

--- addon/utils/listener-name.js
+++ addon/utils/listener-name.js
@@ -1,8 +1,14 @@
+import getCmdKey from 'ember-keyboard/utils/get-cmd-key';
+
 function sortedKeys(keyArray) {
   return keyArray.sort().join('+');
 }
 
 export default function listenerName(type, keyArray = []) {
+  if (keyArray.indexOf('cmd') > -1) {
+    keyArray[keyArray.indexOf('cmd')] = getCmdKey();
+  }
+
   const keys = keyArray.length === 0 ? '_all' : sortedKeys(keyArray);
 
   return `${type}:${keys}`;
