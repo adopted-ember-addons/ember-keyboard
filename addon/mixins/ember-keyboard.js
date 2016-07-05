b0842b4ae9abcc557e83d39b32c867ccb0484cfd
null null null <null@revolution.cheap>
2016-07-05 13:48:00 -0700

    ensure responders are properly unregistered

--- addon/mixins/ember-keyboard.js
+++ addon/mixins/ember-keyboard.js
@@ -16,5 +16,17 @@ export default Mixin.create({
     get(this, 'keyboard').register(this);
 
     return this._super(...args);
+  },
+
+  willDestroyElement(...args) {
+    this._super(...args);
+
+    get(this, 'keyboard').unregister(this);
+  },
+
+  willDestroy(...args) {
+    this._super(...args);
+
+    get(this, 'keyboard').unregister(this);
   }
 });
