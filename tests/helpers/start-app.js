8ed8fe7354df2e3061d642b0a5bf252360f4226a
null null null <null@affinitygamescollective.org>
2017-09-04 12:44:27 -0400

    update deps

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -4,17 +4,14 @@ import config from '../../config/environment';
 import keyboardRegisterTestHelpers from './ember-keyboard/register-test-helpers';
 
 export default function startApp(attrs) {
-  let application;
-
   let attributes = Ember.merge({}, config.APP);
   attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
 
-  Ember.run(() => {
-    application = Application.create(attributes);
+  return Ember.run(() => {
+    let application = Application.create(attributes);
     application.setupForTesting();
     keyboardRegisterTestHelpers();
     application.injectTestHelpers();
+    return application;
   });
-
-  return application;
 }
