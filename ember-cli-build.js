f2e78cca7e5e8a87e9216d08562d944e7112506e
Jan Buschtöns <jan@buschtoens.me>
2017-09-19 15:36:28 +0200

    docs: fix syntax highlighting
    Was accidentally removed in 8ed8fe7354df2e3061d642b0a5bf252360f4226a

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,8 +5,12 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
-    // Add options here
+    babel: {
+      includePolyfill: true
+    }
   });
+
+  app.import('bower_components/highlightjs/styles/monokai-sublime.css');
 
   /*
     This build file specifies the options for the dummy test app of this
