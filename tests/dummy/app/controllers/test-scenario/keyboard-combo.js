9d5c2e953e3b30b40f10f479024f5f85241bab89
Luke Melia <luke@lukemelia.com>
2020-04-16 16:34:28 +0800

    Get tests passing in Ember 3.8

--- tests/dummy/app/controllers/test-scenario/keyboard-combo.js
+++ tests/dummy/app/controllers/test-scenario/keyboard-combo.js
@@ -1,24 +1,18 @@
 import Controller from '@ember/controller';
-import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
 
-export default class extends Controller {
-  @tracked wasCtrlKPressed = false;
-  @tracked wasSPressed = false;
-  @tracked wasSlashPressed = false;
-
-  @action
-  onCtrlK() {
-    this.set('wasCtrlKPressed', true);
+export default Controller.extend({
+  wasCtrlKPressed: false,
+  wasSPressed: false,
+  wasSlashPressed: false,
+  actions: {
+    onCtrlK() {
+      this.set('wasCtrlKPressed', true);
+    },
+    onS() {
+      this.set('wasSPressed', true);
+    },
+    onSlash() {
+      this.set('wasSlashPressed', true);
+    }
   }
-
-  @action
-  onS() {
-    this.set('wasSPressed', true);
-  }
-
-  @action
-  onSlash() {
-    this.set('wasSlashPressed', true);
-  }
-}
+});
