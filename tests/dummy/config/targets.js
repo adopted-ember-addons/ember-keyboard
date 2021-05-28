6df533306aa141b728921acd25b90ad17a7f1768
Luke Melia <luke@lukemelia.com>
2021-05-28 13:32:46 +0800

    Include node in browser targets so fastboot tests pass

--- tests/dummy/config/targets.js
+++ tests/dummy/config/targets.js
@@ -3,7 +3,8 @@
 const browsers = [
   'last 1 Chrome versions',
   'last 1 Firefox versions',
-  'last 1 Safari versions'
+  'last 1 Safari versions',
+  'maintained node versions'
 ];
 
 module.exports = {
