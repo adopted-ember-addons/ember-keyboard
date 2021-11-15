0531219a66ebc4a6559df3bea384288ef72c0394
Sergey Astapov <sergeastapov@gmail.com>
2021-11-14 20:42:54 -0500

    Warn that the disableInputsInitializer configuration can be removed

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  config(environment, appConfig) {
+    const addonConfig = appConfig['emberKeyboard'] || {};
+
+    if ('disableInputsInitializer' in addonConfig) {
+      this.ui.writeDeprecateLine(
+        '[ember-keyboard] The `emberKeyboard.disableInputsInitializer` option is obsolete. ' +
+        'You can remove it from your `config/environment.js` file.', false
+      );
+    }
+  },
 };
