112e2fc54c744755d1b47ecf1622b89e1209225e
Sergey Astapov <sergeastapov@gmail.com>
2021-11-17 00:54:17 -0500

    Make sure disableInputsInitializer deprecation printed only once

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ module.exports = {
   config(environment, appConfig) {
     const addonConfig = appConfig['emberKeyboard'] || {};
 
-    if ('disableInputsInitializer' in addonConfig) {
+    if ('disableInputsInitializer' in addonConfig && !this.deprecationPrinted) {
+      // Do not print deprecation message multiple times
+      // as this hook may be invoked more than once.
+      this.deprecationPrinted = true;
+
       this.ui.writeDeprecateLine(
         '[ember-keyboard] The `emberKeyboard.disableInputsInitializer` option is obsolete. ' +
         'You can remove it from your `config/environment.js` file.', false
