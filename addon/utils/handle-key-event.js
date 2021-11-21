483be2e203527ecd4dfa05b5481926432e066d91
Sergey Astapov <sergeastapov@gmail.com>
2021-11-20 22:38:42 -0500

    Delete deprecated has/trigger responder API

--- addon/utils/handle-key-event.js
+++ addon/utils/handle-key-event.js
@@ -1,25 +1,4 @@
-import getMouseName from 'ember-keyboard/utils/get-mouse-name';
-import listenerName from 'ember-keyboard/utils/listener-name';
 import isKey from 'ember-keyboard/utils/is-key';
-import { deprecate } from '@ember/debug';
-
-function modifierStrings(event) {
-  if (event instanceof KeyboardEvent) {
-    return ['alt', 'ctrl', 'meta', 'shift'].reduce((result, keyName) => {
-      if (event[`${keyName}Key`]) {
-        result.push(keyName);
-      }
-
-      return result;
-    }, []);
-  } else if (event instanceof MouseEvent) {
-    let mouseButton = getMouseName(event.button);
-    if (mouseButton) {
-      return [mouseButton];
-    }
-    return [];
-  }
-}
 
 export function handleKeyEventWithPropagation(
   event,
@@ -100,65 +79,7 @@ function triggerResponderListener(responder, event, ekEvent = null) {
     return;
   }
 
-  if (responder.trigger) {
-    deprecate(
-      'ember-keyboard registered responders handling events via `trigger(listenerName, event)` is deprecated. A responder should have either `keyboardHandlers` (a property returning a dictionary of listenerNames to handler functions), or `handleKeyboardEvent(event)`.',
-      false,
-      {
-        id: 'ember-keyboard.responder-trigger',
-        for: 'ember-keyboard',
-        since: '6.0.2',
-        until: '7.0.0',
-        url: 'https://adopted-ember-addons.github.io/ember-keyboard/deprecations#responder-trigger',
-      }
-    );
-
-    triggerViaLegacyResponderApi(responder, event, ekEvent);
-    return;
-  }
   throw new Error(
     'A responder registered with the ember-keyboard service must implement either `keyboardHandlers` (property returning a dictionary of listenerNames to handler functions), or `handleKeyboardEvent(event)`)'
   );
-}
-
-export function getListenerNames(event) {
-  let result = [];
-  if (event instanceof KeyboardEvent) {
-    if (event.key) {
-      result.push(
-        listenerName(
-          event.type,
-          modifierStrings(event).concat([event.key]).join('+')
-        )
-      );
-    }
-    if (event.code && event.key !== event.code) {
-      result.push(
-        listenerName(
-          event.type,
-          modifierStrings(event).concat([event.code]).join('+')
-        )
-      );
-    }
-  } else if (event instanceof MouseEvent) {
-    let modifiers = modifierStrings(event);
-    if (modifiers.length) {
-      result.push(listenerName(event.type, modifierStrings(event).join('+')));
-    }
-  }
-  result.push(listenerName(event.type));
-  return result;
-}
-
-export function triggerViaLegacyResponderApi(responder, event, ekEvent) {
-  for (const listenerName of getListenerNames(event)) {
-    if (responder.has && !responder.has(listenerName)) {
-      continue;
-    }
-    if (ekEvent) {
-      responder.trigger(listenerName, event, ekEvent);
-    } else {
-      responder.trigger(listenerName, event);
-    }
-  }
 }
