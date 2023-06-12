eee9bb593fabfd09eb3557c9a52eea29e2ddbbfa
Sergey Astapov <SergeAstapov@gmail.com>
2023-06-12 20:11:58 +0000

    add ember v4 LTS to CI

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -60,10 +60,18 @@ module.exports = async function () {
         },
       },
       {
-        name: 'ember-4.0',
+        name: 'ember-lts-4.4',
         npm: {
           devDependencies: {
-            'ember-source': '~4.0.1',
+            'ember-source': '~4.4.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.8',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.8.0',
           },
         },
       },
